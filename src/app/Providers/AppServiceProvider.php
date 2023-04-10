6b42ba48213dfd9476006d23131080051ea8041a
Vyacheslav-Zyatikov <vyacheslav.zyatikov@yandex.ru>
2023-04-10 10:43:37 +0300

    добавление https и некоторые модификации

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -19,6 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       if ($this->isSecure()) {
+        \URL::forceScheme('https');
+       }
+    }
+    
+    public function isSecure()
+    {
+    $isSecure = false;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+        $isSecure = true;
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+        $isSecure = true;
+    }
+
+    return $isSecure;
     }
 }
