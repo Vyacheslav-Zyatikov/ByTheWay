fe27e34dac8e2f417179152b50cf8bd2c818c4db
Vyacheslav-Zyatikov <vyacheslav.zyatikov@yandex.ru>
2023-04-12 21:28:42 +0300

    fix

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,15 +2,12 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/*",
+            input: "resources/js/app.tsx",
         }),
-        basicSsl(),
-        react(),
     ],
     resolve: {
       alias: [
@@ -24,20 +21,14 @@ export default defineConfig({
         },
       ],
     },
-    /* server: {
+     server: {
        host: true,
        port: 3000,
        open: false,
        cors: true,
-       https: true,
-       proxy: {
-            '^(?!(\/\@vite|\/resources|\/node_modules))': {
-                target: `http://77.37.192.93:3000`,
-            }
-        },
        hmr: {
             host: '77.37.192.93',
 		    overlay: true,
         },
-    }, */
+    }, 
 });
